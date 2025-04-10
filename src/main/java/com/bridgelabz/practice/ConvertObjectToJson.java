5cec1e5353da1aa625444f29bffe548c973068bf
LoveleenGoyal <loveleengoyal26@gmail.com>
2025-04-10 09:44:09 +0530

    [Loveleen] ADD: Convert a Java object into JSON

--- src/main/java/com/bridgelabz/practice/ConvertObjectToJson.java
+++ src/main/java/com/bridgelabz/practice/ConvertObjectToJson.java
@@ -1,2 +1,37 @@
-package com.bridgelabz.practice;public class ConvertObjectToJson {
+package com.bridgelabz.practice;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.Arrays;
+import java.util.List;
+
+class Car {
+    public String name;
+    public String model;
+    public int year;
+
+    public Car(String name, String model, int year) {
+        this.name = name;
+        this.model = model;
+        this.year = year;
+    }
+}
+public class ConvertObjectToJson {
+    public static void main(String[] args) {
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        Car car1 = new Car("Tesla", "Model S", 2022);
+        Car car2 = new Car("Ford", "Mustang", 2020);
+        Car car3 = new Car("Toyota", "Camry", 2023);
+
+        List<Car> carList = Arrays.asList(car1, car2, car3);
+
+        try {
+            String json = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(carList);
+            System.out.println(json);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+    }
 }
