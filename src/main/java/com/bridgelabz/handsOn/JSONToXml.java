40b3cfa1492b3cadbbc0021e6d6d92e466cc06b8
LoveleenGoyal <loveleengoyal26@gmail.com>
2025-04-11 10:56:07 +0530

    [Loveleen] ADD: Convert JSON to Xml

--- src/main/java/com/bridgelabz/handsOn/JSONToXml.java
+++ src/main/java/com/bridgelabz/handsOn/JSONToXml.java
@@ -1,2 +1,19 @@
-package com.bridgelabz.handsOn;public class JSONToXml {
+package com.bridgelabz.handsOn;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+
+public class JSONToXml {
+    public static void main(String[] args) throws JsonProcessingException {
+        String json = "{\"name\":\"Love\", \"email\":\"love@example.com\"}";
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode jsonNode = objectMapper.readTree(json);
+
+        XmlMapper xmlMapper = new XmlMapper();
+        String xml = xmlMapper.writeValueAsString(jsonNode);
+        System.out.println(xml);
+    }
 }
